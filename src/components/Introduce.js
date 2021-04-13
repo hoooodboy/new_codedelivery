22dfd9cef1630642fd739a6dc122d1ef2f2acb61
031hoodboy <littlepark0107@gmail.com>
2021-04-13 16:51:53 +0900

    feat: add introduce image

--- src/components/Introduce.js
+++ src/components/Introduce.js
@@ -12,6 +12,7 @@ const IntroduceBackgroundBlock = styled.div`
   display: flex;
   flex-direction:column;
   align-items: center;
+  justify-content: center;
 `;
 
 const Name = styled.div`
@@ -49,8 +50,10 @@ const Explain = styled.div`
 
 const IntroduceImgBlock = styled.div`
     width: 100%;
-
     height: 100%;
+    min-width: 250px;
+    min-height: 250px;
+    max-width: 650px;
     background-image: url(${IntroduceImg});
     background-size: contain;
     background-repeat: no-repeat;
